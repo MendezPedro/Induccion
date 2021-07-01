320faac9f3d5644b8de71af8a96eeb4c2a211712
MendezPedro <mendez.pedro94@gmail.com>
2021-07-01 16:45:24 -0400

    POO sin depurar

--- app.py
+++ app.py
@@ -1,37 +1,64 @@
 import xmlrpc.client
 
-#configuration
 url = 'http://52.142.63.20:1269'
 db = 'fraccion_test'
 username = 'WSadnet@adnetworks.cl'
 password = 'WSadnet@adnetworks.cl'
 
+class consulta():
 
-#logging
-common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
-common.version()
+    def __init__(self):
+        self.url = url
+        self.db = db
+        self.username = username
+        self.password = password
+        print(url)
 
-uid = common.authenticate(db, username, password, {})
+    def logging(self):
 
-#calling methods for "execute_kw"
-models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
-models.execute_kw(db, uid, password,
-    'res.partner', 'check_access_rights',
-    ['read'], {'raise_exception': False})
+        common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
+        common.version()
+        uid = common.authenticate(db, username, password, {})
+        return uid
 
-#search_count
-search_count = models.execute_kw(db, uid, password,
-    'res.partner', 'search_count',
-    [[['is_company', '=', True], ['customer', '=', True]]])
+    def models_id(self):
+        
+        usuario_id = information.logging()
+        
+        models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
+        models.execute_kw(db, usuario_id, password,
+            'res.partner', 'check_access_rights',
+            ['read'], {'raise_exception': False})
+        return models
+        
 
-print(search_count)
+    def search_count(self):
+        
+        uid = information.logging()
+        models = information.models_id()
 
-#search and read
-search_read = models.execute_kw(db, uid, password,
-    'res.partner', 'search_read',
-    [[['is_company', '=', True], ['customer', '=', True]]],
-    {'fields': ['name', 'country_id', 'comment'], 'limit': 5})
+        s_count = models.execute_kw(db, uid, password,
+            'res.partner', 'search_count',
+            [[['is_company', '=', True], ['customer', '=', True]]])
+        return s_count
 
+
+    def search_read(self):
+        uid = information.logging()
+        models = information.models_id()
+        
+        s_read = models.execute_kw(db, uid, password,
+            'res.partner', 'search_read',
+            [[['is_company', '=', True], ['customer', '=', True]]],
+            {'fields': ['name', 'country_id', 'comment'], 'limit': 5})
+        return s_read
+
+
+information = consulta()
+print("imprime search count: ", information.search_count())
+
+search_read = information.search_read()
+print("imprime search read: ", search_read)
 
 #devuelve llaves en español
 lista = []
@@ -42,6 +69,7 @@ for element in search_read:
     dicc['pais_id'] = element['country_id']
     dicc['comentarios'] = element['comment']
     lista.append(dicc)
+
 
 print(lista)
 
